7a14174cd100f7eda8f79a933e1cfa1fcc94c95c
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-16 16:35:29 +0700

    Update new

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="h-[60px] bg-green-600 text-white flex items-center px-10">
-      <h1 className="text-lg font-bold">An Thanh Food 0</h1>
+      <h1 className="text-lg font-bold">An Thanh Food</h1>
     </header>
   );
 };
