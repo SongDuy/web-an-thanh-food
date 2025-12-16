50a96c927cfcfd8cb36568a1412f3fa44ed28d84
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-16 20:23:57 +0700

    Update new

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header className="w-full h-[100px] bg-white text-black flex items-center px-10">
-      <h1 className="text-lg font-bold">An Thanh Food</h1>
+    <header className="w-full h-[100px] bg-white text-black flex items-center px-[60px]">
+      {/* logo web */}
+      <img
+        src={``}
+        alt="An Thanh Food"
+        className="w-[75px] mr-3 bg-gray-200"
+      />
+
+      {/* Danh mục header */}
+      <nav aria-label="Main navigation">
+        <ul className="flex overflow-x-auto gap-5 mx-3 whitespace-nowrap">
+          <li>
+            <Link
+              to="/"
+              className="uppercase text-lg font-bold text-10 hover:text-yellow-500">
+              Trang Chủ
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className="uppercase text-lg font-bold text-10 hover:text-yellow-500">
+              An Thanh Food
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className="uppercase text-lg font-bold text-10 hover:text-yellow-500">
+              An Thanh Food
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Các nút chức năng */}
+
     </header>
   );
 };
