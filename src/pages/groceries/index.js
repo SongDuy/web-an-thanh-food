6c8ba7f74c6f6674b9248f3959fae84470248a30
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-17 20:32:24 +0700

    Update new

---
+++ src/pages/groceries/index.js
@@ -0,0 +1,16 @@
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const GroceriesPage = () => {
+    return (
+        <>
+            <Header />
+            <div>
+
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default GroceriesPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from '../pages/home';
 import NotFoundPage from '../pages/notFound';
+import StaplesPage from '../pages/staples';
+import GroceriesPage from '../pages/groceries';
 
 const AppRoutes = () => {
     return (
         <Routes>
             {/* Trang chủ */}
             <Route path="/" element={<HomePage />} />
+            <Route path="/staples" element={<StaplesPage />} />
+            <Route path="/groceries" element={<GroceriesPage />} />
 
             {/* Trang 404 */}
             <Route path="/404" element={<NotFoundPage />} />
