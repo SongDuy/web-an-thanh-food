81dbd8b03f564d5232d66a13df4555f5d98e88b9
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-17 14:11:12 +0700

    Update new

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,13 +4,15 @@ import ProductCard from '../../components/ProductCard';
 const HomePage = () => {
     return (
         <MainLayout>
-            <h1 className="text-2xl font-bold mb-6">Sản phẩm nổi bật</h1>
+            <div>
+                <h1 className="text-2xl font-bold mb-6">Sản phẩm nổi bật</h1>
 
-            <div className="grid grid-cols-4 gap-6">
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                <div className="grid grid-cols-4 gap-6">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
             </div>
         </MainLayout>
     );
