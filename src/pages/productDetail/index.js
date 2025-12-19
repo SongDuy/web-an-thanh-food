acb888a80f72fb61ddc47e35d343be14586c00a5
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-20 06:49:51 +0700

    Update new

--- src/pages/productDetail/index.js
+++ src/pages/productDetail/index.js
@@ -1,5 +1,7 @@
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { Link } from 'react-router-dom';
 
 const ProductDetailPage = () => {
 
@@ -8,7 +10,30 @@ const ProductDetailPage = () => {
             <Header />
             <div className="w-full h-full bg-gray-100 px-[160px] border pb-[45px]">
                 {/* Hình ảnh sản phẩm và đặt hàng */}
-                <div className="w-full h-[600px] mt-[35px] px-[25px] py-[25px] border rounded-lg bg-white grid grid-cols-5 gap-5">
+                <div className="w-full h-[35px] flex items-center">
+                    <Link to="/">
+                        <span className="text-blue-800 text-sm">
+                            Trang chủ
+                        </span>
+                    </Link>
+
+                    <NavigateNextIcon className="relative top-[2px]" fontSize="small" />
+
+                    <Link to="/Lương-Thực">
+                        <span className="text-blue-800 text-sm">
+                            Lương thực
+                        </span>
+                    </Link>
+
+                    <NavigateNextIcon className="relative top-[2px]" fontSize="small" />
+
+                    <span className="text-black text-sm">
+                        Chi tiết
+                    </span>
+
+                </div>
+
+                <div className="w-full h-[600px] px-[25px] py-[25px] border rounded-lg bg-white grid grid-cols-5 gap-5">
                     {/* 1/3 */}
                     <div className="col-span-2 bg-gray-100">
                         Nội dung 2/5
@@ -22,12 +47,12 @@ const ProductDetailPage = () => {
 
 
                 {/* Thông tin mô tả của sản phẩm */}
-                <div className=" w-full h-[550px] mt-[35px] px-[25px] py-[25px] border rounded-lg bg-white">
+                <div className=" w-full h-[550px] mt-[15px] px-[25px] py-[25px] border rounded-lg bg-white">
                     kkkk
                 </div>
 
                 {/* Phần bình luận phản hồi */}
-                <div className=" w-full h-[550px] mt-[35px] px-[25px] py-[25px] border rounded-lg bg-white">
+                <div className=" w-full h-[550px] mt-[15px] px-[25px] py-[25px] border rounded-lg bg-white">
                     kkkk
                 </div>
             </div>
