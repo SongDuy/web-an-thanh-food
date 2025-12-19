dc5678e7bbb499dd5f83dfad9f73850d5bb278fd
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-19 21:46:50 +0700

    Update new

--- src/pages/productDetail/index.js
+++ src/pages/productDetail/index.js
@@ -7,10 +7,27 @@ const ProductDetailPage = () => {
         <>
             <Header />
             <div className="w-full h-full bg-gray-100 px-[160px] border pb-[45px]">
-                <div className=" w-full h-[550px] mt-[45px] border bg-white">
+                {/* Hình ảnh sản phẩm và đặt hàng */}
+                <div className="w-full h-[550px] mt-[25px] px-[25px] py-[25px] border rounded-lg bg-white grid grid-cols-5 gap-4">
+                    {/* 1/3 */}
+                    <div className="col-span-2 bg-gray-100">
+                        Nội dung 2/5
+                    </div>
+
+                    {/* 2/3 */}
+                    <div className="col-span-3 bg-gray-200">
+                        Nội dung 3/5
+                    </div>
+                </div>
+
+
+                {/* Thông tin mô tả của sản phẩm */}
+                <div className=" w-full h-[550px] mt-[25px] px-[25px] py-[25px] border rounded-lg bg-white">
                     kkkk
                 </div>
-                <div className=" w-full h-[630px] mt-[45px] border bg-white">
+
+                {/* Phần bình luận phản hồi */}
+                <div className=" w-full h-[550px] mt-[25px] px-[25px] py-[25px] border rounded-lg bg-white">
                     kkkk
                 </div>
             </div>
