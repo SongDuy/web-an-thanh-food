dac8f4c60e56809bff38e2ad610b59aeef2a5a61
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-19 17:30:00 +0700

    Update new

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,12 +9,21 @@ const ProductCard = () => {
       <div className="border p-4 rounded hover:shadow-md hover:bg-red-50 transition-all duration-300 bg-white max-w-[230px] h-[320px] flex flex-col">
 
         {/* Khu vực ảnh: Giữ nguyên h-40 (~160px) */}
-        <div className="w-full h-[160px] mb-3 overflow-hidden rounded flex-shrink-0">
+        <div className=" relative w-full h-[160px] mb-3 overflow-hidden rounded flex-shrink-0">
           <img
             src={productImg}
             alt="Rau cải xanh"
             className="w-full h-full object-cover transition-transform duration-500"
           />
+          <div className="absolute top-0 right-0 bg-yellow-500 text-white font-bold text-shadow-black px-3 py-1 rounded-bl-xl text-md  shadow-md z-10">
+            <span className="text-md">
+              0
+            </span>
+            <span className="text-sm">
+              đ
+            </span>
+
+          </div>
         </div>
 
         {/* Nội dung text: flex-grow giúp vùng này chiếm không gian còn lại */}
@@ -26,7 +35,7 @@ const ProductCard = () => {
         {/* Nút bấm: Sẽ luôn nằm ở đáy nhờ cấu trúc flex-col */}
         <div className="mt-auto grid grid-cols-2 gap-3 w-full">
           <button className=" bg-gray-200 hover:bg-gray-300 border border-gray-300 text-black text-md px-1 py-1 rounded transition-colors font-medium ">
-            <ShoppingCartOutlinedIcon /> 
+            <ShoppingCartOutlinedIcon />
             100000
           </button>
           {/* <button className="bg-green-500 hover:bg-green-600 text-white text-md px-1 py-1 rounded transition-colors font-medium ">
