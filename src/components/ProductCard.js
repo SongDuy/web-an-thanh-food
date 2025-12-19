682534a1e8e2c80c8951a6ca6ea22e8794da7a14
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-19 15:09:21 +0700

    Update new

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
 const ProductCard = () => {
   const productImg = "https://images.unsplash.com/photo-1518843875459-f738682238a6?q=80&w=2042&auto=format&fit=crop";
 
   return (
     <Link to="/Lương-Thực/Chi-Tiết">
-      <div className="border p-4 rounded hover:shadow-md hover:bg-red-50 transition-all duration-300 bg-white max-w-[250px] h-[320px] flex flex-col">
+      <div className="border p-4 rounded hover:shadow-md hover:bg-red-50 transition-all duration-300 bg-white max-w-[230px] h-[320px] flex flex-col">
 
         {/* Khu vực ảnh: Giữ nguyên h-40 (~160px) */}
         <div className="w-full h-[160px] mb-3 overflow-hidden rounded flex-shrink-0">
@@ -23,11 +25,14 @@ const ProductCard = () => {
 
         {/* Nút bấm: Sẽ luôn nằm ở đáy nhờ cấu trúc flex-col */}
         <div className="mt-auto grid grid-cols-2 gap-3 w-full">
-          <button className="bg-white border border-green-500 text-green-600 hover:bg-green-50 px-3 py-2 rounded transition-colors font-medium text-sm">
-            Thêm vào
+          <button className=" bg-gray-200 hover:bg-gray-300 border border-gray-200 text-black text-md px-1 py-1 rounded transition-colors font-medium ">
+            <ShoppingCartOutlinedIcon/> 100000
           </button>
-          <button className="bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded transition-colors font-medium text-sm">
+          {/* <button className="bg-green-500 hover:bg-green-600 text-white text-md px-1 py-1 rounded transition-colors font-medium ">
             Mua ngay
+          </button> */}
+          <button className="bg-red-500 hover:bg-red-600 text-white text-md px-1 py-1 rounded transition-colors font-medium ">
+            Hết hàng
           </button>
         </div>
       </div>
