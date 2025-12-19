3c375171c552532b88016f85a0e980f9efd53d8a
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-19 17:34:27 +0700

    Update new

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -15,7 +15,7 @@ const ProductCard = () => {
             alt="Rau cải xanh"
             className="w-full h-full object-cover transition-transform duration-500"
           />
-          <div className="absolute top-0 right-0 bg-yellow-500 text-white font-bold text-shadow-black px-3 py-1 rounded-bl-xl text-md  shadow-md z-10">
+          <div className="absolute top-0 right-0 bg-yellow-500 text-white font-bold text-shadow-black px-3 py-1 rounded-bl-xl z-10">
             <span className="text-md">
               0
             </span>
