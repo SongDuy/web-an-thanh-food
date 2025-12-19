85d589ecc5925bc341ac2e1f31423d77c581d6fc
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-19 22:36:23 +0700

    Update new

--- src/pages/productDetail/index.js
+++ src/pages/productDetail/index.js
@@ -8,7 +8,7 @@ const ProductDetailPage = () => {
             <Header />
             <div className="w-full h-full bg-gray-100 px-[160px] border pb-[45px]">
                 {/* Hình ảnh sản phẩm và đặt hàng */}
-                <div className="w-full h-[600px] mt-[15px] px-[25px] py-[25px] border rounded-lg bg-white grid grid-cols-5 gap-5">
+                <div className="w-full h-[600px] mt-[35px] px-[25px] py-[25px] border rounded-lg bg-white grid grid-cols-5 gap-5">
                     {/* 1/3 */}
                     <div className="col-span-2 bg-gray-100">
                         Nội dung 2/5
@@ -22,12 +22,12 @@ const ProductDetailPage = () => {
 
 
                 {/* Thông tin mô tả của sản phẩm */}
-                <div className=" w-full h-[550px] mt-[15px] px-[25px] py-[25px] border rounded-lg bg-white">
+                <div className=" w-full h-[550px] mt-[35px] px-[25px] py-[25px] border rounded-lg bg-white">
                     kkkk
                 </div>
 
                 {/* Phần bình luận phản hồi */}
-                <div className=" w-full h-[550px] mt-[15px] px-[25px] py-[25px] border rounded-lg bg-white">
+                <div className=" w-full h-[550px] mt-[35px] px-[25px] py-[25px] border rounded-lg bg-white">
                     kkkk
                 </div>
             </div>
