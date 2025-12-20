97ae65519fe0ac465545bc76ffdfc45a69d1f4a5
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-20 19:22:00 +0700

    Update new

--- src/pages/productDetail/index.js
+++ src/pages/productDetail/index.js
@@ -76,7 +76,7 @@ const ProductDetailPage = () => {
                             </h1>
                         </div>
                         <div className="w-full h-[65px] bg-gray-100 rounded">
-                            <h1 className="w-full h-full flex items-center justify-center text-red-500 text-shadow-black font-bold">
+                            <h1 className="w-full h-full flex items-center justify-center text-red-100 text-shadow-black font-bold">
                                 <span className="text-[40px] tracking-tight">0</span>
                                 <span className="text-[20px] ml-1 underline underline-offset-2 relative -top-[4px]">
                                     đ
