f1ef5690bbe6fc0231f7e397f7f2bed049f682e5
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-22 08:18:34 +0700

    Update new

--- src/pages/staples/index.js
+++ src/pages/staples/index.js
@@ -1,11 +1,24 @@
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import ProductCard from '../../components/ProductCard';
+
 const StaplesPage = () => {
     return (
         <>
             <Header />
-            <div className="px-[160px]">
-
+            <div className="px-[160px] py-[45px]">
+                <div className="w-full min-h-[650px] grid grid-cols-5 gap-3 mt-[35px]">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
             </div>
             <Footer />
         </>
