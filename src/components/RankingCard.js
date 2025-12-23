6f8b9d7177fb8d32a7f520bb9a635e597cb19f9d
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-23 19:46:46 +0700

    Update new

--- src/components/RankingCard.js
+++ src/components/RankingCard.js
@@ -1,7 +1,21 @@
 const RankingCard = () => {
   return (
     <div className="w-full h-[120px] border rounded shadow hover:bg-red-50 p-4">
-        kkk
+      <div className="w-full h-full flex gap-3">
+        <div className="min-w-[90px] h-full border">
+
+          <span>kkkk</span>
+        </div>
+        <div className="w-full h-full grid grid-cols-[70%_30%] gap-3">
+          <div className="w-full border">
+
+          </div>
+          <div className="w-full border">
+          </div>
+
+
+        </div>
+      </div>
     </div>
   );
 };
