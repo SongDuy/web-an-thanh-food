f4f09121e0d1b9e49ce98422dcc0b32aab0fa1a6
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-23 07:31:30 +0700

    Update new

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import BannerSlider from '../components/BannerSlider';
+import Search from '../components/Search';
 
 const MainLayout = ({ children }) => {
+    const [openSearch, setOpenSearch] = useState(false);
+
     return (
         <>
-            <Header />
+            <Header onOpenSearch={() => setOpenSearch(true)} />
+
+            {openSearch && (
+                <Search onClose={() => setOpenSearch(false)} />
+            )}
 
             <BannerSlider />
 
