e724abe58c7d0789adf65efd83e08faba8a2912c
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-23 19:53:47 +0700

    Update new

--- src/components/RankingCard.js
+++ src/components/RankingCard.js
@@ -1,11 +1,17 @@
 const RankingCard = () => {
+  const productImg = "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png";
+
   return (
     <div className="w-full h-[120px] border rounded shadow hover:bg-red-50 p-4">
       <div className="w-full h-full flex gap-3">
         <div className="min-w-[90px] h-full border">
-
-          <span>kkkk</span>
+          <img
+            src={productImg}
+            alt="Rau cải xanh"
+            className="w-full h-full object-cover transition-transform duration-500"
+          />
         </div>
+
         <div className="w-full h-full grid grid-cols-[70%_30%]">
           <div className="w-full border">
 
@@ -13,8 +19,6 @@ const RankingCard = () => {
           <div className="w-full border">
 
           </div>
-
-
         </div>
       </div>
     </div>
