0b32eb9efae82399794eddbff550e788277721f7
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-23 21:01:41 +0700

    Update new

--- src/components/RankingCard.js
+++ src/components/RankingCard.js
@@ -1,3 +1,6 @@
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
 const RankingCard = () => {
   const productImg = "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png";
 
@@ -20,17 +23,36 @@ const RankingCard = () => {
         </div>
 
         <div className="w-full h-full grid grid-cols-[75%_25%]">
-          <div className="w-full flex-grow pr-3">
-            <h3 className="w-full text-md font-semibold text-gray-800 line-clamp-1">Rau cải xanh Rau cải xanh Rau cải xanh Rau cải xanh Rau cải xanh</h3>
-            <p className="text-sm font-semibold text-red-300">1 gói / 1kg</p>
+          <div className="w-full flex flex-col pr-3">
+            <h3 className="w-full text-md font-semibold text-gray-800 line-clamp-1">
+              Rau cải xanh Rau cải xanh Rau cải xanh Rau cải xanh Rau cải xanh
+            </h3>
+
+            <p className="text-sm font-semibold text-red-300">
+              1 gói / 1kg
+            </p>
+
+            {/* Buttons sát đáy */}
+            <div className="w-full mt-auto grid grid-cols-2">
+              <button className="pr-3 border-r text-black flex items-center justify-center gap-1">
+                <StarBorderOutlinedIcon fontSize="small" />
+                Đánh Giá
+              </button>
+
+              <button className="pl-3 border-l text-black flex items-center justify-center gap-1">
+                <FavoriteBorderOutlinedIcon fontSize="small" />
+                Yêu Thích
+              </button>
+            </div>
           </div>
 
           <div className="w-full flex items-center">
-            <button className="w-full h-[35px] bg-green-500 hover:bg-green-600 shadow text-white text-md px-1 py-1 rounded transition-colors font-medium ">
+            <button className="w-full h-[35px] bg-green-500 hover:bg-green-600 shadow text-white text-md px-1 py-1 rounded transition-colors font-medium">
               Mua ngay
             </button>
           </div>
         </div>
+
       </div>
     </div>
   );
