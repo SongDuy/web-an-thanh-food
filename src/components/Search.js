7789cd75d32479b6ca7d13b2656528b1bb83bbdb
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-24 22:06:21 +0700

    Update new

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,256 @@
 import { useState } from "react";
+
 import CloseIcon from "@mui/icons-material/Close";
-import SearchCard from "./SearchCard";
+import StarIcon from "@mui/icons-material/Star";
+import InventoryIcon from "@mui/icons-material/Inventory";
+
+const products = [
+    {
+        id: 1,
+        name: "Sữa Tươi Tiệt Trùng - Có Đường",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Vinamilk",
+        origin: "Việt Nam",
+        expiryDate: "2026-06-30",
+        rating: 4.5,
+        likes: 125,
+        stock: 3200
+    },
+    {
+        id: 2,
+        name: "Sữa Tươi Tiệt Trùng - Ít Đường",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Vinamilk",
+        origin: "Việt Nam",
+        expiryDate: "2026-06-30",
+        rating: 4.5,
+        likes: 125,
+        stock: 0
+    },
+    {
+        id: 3,
+        name: "Sữa Tươi Tiệt Trùng - Không Đường",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Vinamilk",
+        origin: "Việt Nam",
+        expiryDate: "2026-07-15",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 4,
+        name: "Sữa Tươi Tiệt Trùng - Vị Dâu",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Vinamilk",
+        origin: "Việt Nam",
+        expiryDate: "2026-08-01",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 5,
+        name: "Sữa Tươi Tiệt Trùng - Vị Socola",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Vinamilk",
+        origin: "Việt Nam",
+        expiryDate: "2026-08-01",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 6,
+        name: "Mì - Tôm Chua Cay",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 gói",
+        weight: "1 kg",
+        brand: "Hảo Hảo",
+        origin: "Việt Nam",
+        expiryDate: "2025-12-31",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 7,
+        name: "Sữa Đậu Nành - Có Đường",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Fami",
+        origin: "Việt Nam",
+        expiryDate: "2026-05-20",
+        rating: 4.5,
+        likes: 125,
+        stock: 0
+    },
+    {
+        id: 8,
+        name: "Sữa Đậu Nành - Ít Đường",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Fami",
+        origin: "Việt Nam",
+        expiryDate: "2026-05-20",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 9,
+        name: "Sữa Đậu Nành - Không Đường",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Fami",
+        origin: "Việt Nam",
+        expiryDate: "2026-05-20",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 10,
+        name: "Gạo Lứt - Huyết Rồng",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Lương thực",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "ST",
+        origin: "Sóc Trăng, Việt Nam",
+        expiryDate: "2027-01-15",
+        rating: 4.5,
+        likes: 125,
+        stock: 0
+    },
+    {
+        id: 11,
+        name: "Gạo Lứt - Tím Than",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Lương thực",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "ST",
+        origin: "Sóc Trăng, Việt Nam",
+        expiryDate: "2027-01-15",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 12,
+        name: "Gạo - ST25",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Lương thực",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "ST",
+        origin: "Sóc Trăng, Việt Nam",
+        expiryDate: "2027-02-01",
+        rating: 4.5,
+        likes: 125,
+        stock: 0
+    },
+    {
+        id: 13,
+        name: "Gạo - ST21",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Lương thực",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "ST",
+        origin: "Sóc Trăng, Việt Nam",
+        expiryDate: "2027-02-01",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 14,
+        name: "Thịt Hộp - Hàn Quốc",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 hộp",
+        weight: "1 kg",
+        brand: "CJ",
+        origin: "Hàn Quốc",
+        expiryDate: "2026-12-31",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    },
+    {
+        id: 15,
+        name: "Cá Hộp - Sốt Cà",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Thực phẩm",
+        unit: "1 hộp",
+        weight: "1 kg",
+        brand: "Hạ Long",
+        origin: "Việt Nam",
+        expiryDate: "2026-11-30",
+        rating: 4.5,
+        likes: 125,
+        stock: 0
+    },
+    {
+        id: 16,
+        name: "Đậu Đen Xanh Lòng",
+        image: "https://product.hstatic.net/1000141988/product/sua_tuoi_tiet_trung_co_duong_vinamilk_viet_nam__1l__2f553e41e7f54abba37116456aa94db3_grande.png",
+        price: 0,
+        category: "Lương thực",
+        unit: "1 túi",
+        weight: "1 kg",
+        brand: "Nông sản Việt",
+        origin: "Việt Nam",
+        expiryDate: "2027-03-10",
+        rating: 4.5,
+        likes: 125,
+        stock: 32
+    }
+];
 
 const Search = ({ onClose }) => {
     const [isClosing, setIsClosing] = useState(false);
+    const [keyword, setKeyword] = useState("");
+    const hasKeyword = keyword.trim().length > 0;
 
     const handleClose = () => {
         setIsClosing(true);
@@ -13,6 +260,10 @@ const Search = ({ onClose }) => {
             onClose();
         }, 1000); // = duration trong CSS
     };
+
+    const filteredProducts = products.filter((item) =>
+        item.name.toLowerCase().includes(keyword.toLowerCase())
+    );
 
     return (
         <div
@@ -45,30 +296,77 @@ const Search = ({ onClose }) => {
                 {/* Search input */}
                 <div className="mt-5">
                     <input
+                        value={keyword}
+                        onChange={(e) => setKeyword(e.target.value)}
                         className="w-full h-10 px-3 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-black shadow-sm"
                         placeholder="Tìm sản phẩm..."
                     />
-
-                    {/* Tabs */}
-                    {/* <div className="mt-[25px] flex gap-3">
-                        <button className="flex-1 h-9 text-sm font-medium rounded-lg bg-black text-white transition">
-                            Lương Thực
-                        </button>
-                        <button className="flex-1 h-9 text-sm font-medium rounded-lg bg-gray-100 hover:bg-gray-200 shadow border text-gray-700  transition">
-                            Thực Phẩm
-                        </button>
-                    </div> */}
                 </div>
 
                 {/* Result list */}
-                <div className="w-full grid grid-cols-1 gap-2 mt-[25px]">
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
-                    <SearchCard />
+                <div className="w-full grid grid-cols-1 gap-2 mt-[25px] overflow-y-auto custom-scrollbar">
+
+                    {!hasKeyword && (
+                        <p className="text-sm text-gray-400 text-center mt-10">
+                            Nhập tên sản phẩm để tìm kiếm
+                        </p>
+                    )}
+
+                    {hasKeyword && filteredProducts.length === 0 && (
+                        <p className="text-sm text-gray-400 text-center mt-5">
+                            Không tìm thấy sản phẩm
+                        </p>
+                    )}
+
+                    {hasKeyword && filteredProducts.map((product) => (
+                        <div
+                            key={product.id}
+                            className="flex items-center gap-3 p-2 rounded-lg border shadow hover:bg-red-50 cursor-pointer transition"
+                        >
+                            {/* Thumbnail */}
+                            <img
+                                src={product.image}
+                                alt={product.name}
+                                className="w-[70px] h-[70px] rounded-lg object-cover flex-shrink-0"
+                            />
+
+                            {/* Info */}
+                            <div className="flex-1 overflow-hidden">
+                                <h3 className="text-sm font-semibold text-gray-800 line-clamp-1">
+                                    {product.name}
+                                </h3>
+
+                                <div className="flex text-xs mt-0.5">
+                                    <span className="pr-2 text-red-300 font-bold">
+                                        {product.category}
+                                    </span>
+                                    <span className="mx-2 h-3 w-px bg-gray-300" />
+                                    <span className="text-red-300 font-semibold">
+                                        {product.unit} * {product.weight}
+                                    </span>
+                                </div>
+
+                                <div className="flex items-center gap-3 text-xs font-semibold mt-1">
+                                    <div className="flex items-center gap-1 text-yellow-500">
+                                        <StarIcon fontSize="inherit" />
+                                        {product.rating}
+                                    </div>
+
+                                    <div
+                                        className={`flex items-center gap-1 ${product.stock === 0
+                                                ? "text-gray-400"
+                                                : "text-green-600"
+                                            }`}
+                                    >
+                                        <InventoryIcon fontSize="inherit" />
+                                        {product.stock === 0 ? "Hết hàng" : "Mua ngay"}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
+
             </div>
         </div>
     );
