23d5d62acfc673abda56b967a03a96ce6c871ecb
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-26 16:01:12 +0700

    Update new

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -9,6 +9,17 @@ const CartPage = () => {
 
       <div className="w-full bg-gray-100 px-[160px] pt-[100px] pb-[45px]">
         <h1 className="text-[22px] my-[35px] font-bold uppercase"> Giỏ Hàng </h1>
+        
+        <div>
+          <div>
+
+          </div>
+
+          <div>
+
+          </div>
+        </div>
+
       </div>
 
       <Footer />
