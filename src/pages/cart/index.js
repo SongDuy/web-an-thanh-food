5de7743a8c663b7578f2dbf7ab8985251ca25436
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2025-12-27 07:56:23 +0700

    Update new

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -29,20 +29,20 @@ const CartPage = () => {
         <div className="w-full grid grid-cols-6 gap-5">
           <div className="col-span-4">
             <div className="w-full bg-white rounded-md px-3 py-2">
-              <div className="w-full grid grid-cols-[5fr_1.5fr_1.5fr_1.5fr_0.5fr] gap-3">
+              <div className="w-full grid grid-cols-[5.5fr_1.5fr_1.3fr_1.2fr_0.5fr] gap-2 pl-2.5">
                 <div className="w-full flex items-center">
-                  <span> Tên sản phẩm </span>
+                  <span> Tất cả (1 sản phẩm)</span>
                 </div>
 
                 <div className="w-full flex items-center justify-center">
                   <span> Đóng gói </span>
                 </div>
 
-                <div className="w-full flex items-center justify-center">
+                <div className="w-full flex items-center justify-center border">
                   <span> Số lượng </span>
                 </div>
 
-                <div className="w-full flex items-center justify-center">
+                <div className="w-full flex items-center justify-center border">
                   <span> Giá tiền </span>
                 </div>
 
@@ -52,37 +52,44 @@ const CartPage = () => {
               </div>
             </div>
 
-            <div className="w-full h-[500px] mt-[15px] bg-white rounded-md px-3 py-3">
-              <div className="w-full grid grid-cols-[5fr_1.5fr_1.5fr_1.5fr_0.5fr] gap-3">
-                <div className="w-full flex items-center">
-                  Tên sản phẩm
-                </div>
+            <div className="w-full min-h-[500px] mt-[15px] bg-white rounded-md px-3 py-3">
+              <div className="w-full grid grid-cols-1 gap-5 py-3 pl-2.5">
 
-                <div className="w-full flex items-center justify-center">
-                  3 túi * 1 kg
-                </div>
+                {/* Sẳm phẩm */}
+                <div className="w-full h-[100px] grid grid-cols-[5.5fr_1.5fr_1.3fr_1.2fr_0.5fr] gap-2 ">
+                  <div className="w-full flex items-center border">
+                    <div className="w-[90px] h-[90px] bg-gray-100">
 
-                <div className="w-full flex items-center justify-center text-gray-400">
-                  <div className="flex border-2 rounded overflow-hidden">
-                    <div className="w-8 h-[28px] flex items-center justify-center font-bold border-r">
-                      <RemoveOutlinedIcon sx={{ fontSize: 20 }} />
                     </div>
-                    <div className="w-8 h-[28px] text-md font-semibold flex items-center justify-center border-l">
-                      1
+                  </div>
+
+                  <div className="w-full flex items-center justify-center border">
+                    3 túi * 1 kg
+                  </div>
+
+                  <div className="w-full flex items-center justify-center text-gray-400 border">
+                    <div className="flex border-2 rounded overflow-hidden">
+                      <div className="w-6 h-[22px] flex items-center justify-center font-bold border-r">
+                        <RemoveOutlinedIcon sx={{ fontSize: 20 }} />
+                      </div>
+                      <div className="w-8 h-[22px] text-md font-semibold flex items-center justify-center border-l">
+                        1
+                      </div>
+                      <div className="w-6 h-[22px] flex items-center justify-center font-bold border-l">
+                        <AddOutlinedIcon sx={{ fontSize: 20 }} />
+                      </div>
                     </div>
-                    <div className="w-8 h-[28px] flex items-center justify-center font-bold border-l">
-                      <AddOutlinedIcon sx={{ fontSize: 20 }} />
-                    </div>
+                  </div>
+
+                  <div className="w-full flex items-center justify-center text-lg font-bold text-red-500 border">
+                    0đ
+                  </div>
+
+                  <div className="w-full flex items-center justify-center border">
+                    <DeleteOutlineOutlinedIcon />
                   </div>
                 </div>
 
-                <div className="w-full flex items-center justify-center text-lg font-bold text-red-500">
-                  0đ
-                </div>
-
-                <div className="w-full flex items-center justify-center">
-                  <DeleteOutlineOutlinedIcon />
-                </div>
               </div>
             </div>
           </div>
