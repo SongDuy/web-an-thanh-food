1aa0e8990ac4f2b2bcde6aa64ffee2072c7a8b13
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2026-01-03 19:28:49 +0700

    Update new

--- src/components/Header.js
+++ src/components/Header.js
@@ -122,7 +122,7 @@ const Header = ({ onOpenSearch, onOpenNotify }) => {
 
           {/* Dropdown */}
           {openAccount && (
-            <div className="absolute right-0 mt-2 w-56 bg-white border rounded-xl shadow-lg z-50">
+            <div className="absolute right-0 mt-2 w-56 bg-white border rounded-xl shadow z-50">
               <ul className="px-2 py-2 text-sm flex flex-col">
 
                 <li className="flex w-full h-[50px] mb-2 border border-b-2 border-gray-100 rounded-md bg-white shadow items-center justify-center gap-2">
