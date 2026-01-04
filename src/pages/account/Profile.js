9ff9a0edc1ba682fa087a33ef869e77fed864c66
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2026-01-04 08:40:12 +0700

    Update new

--- src/pages/account/Profile.js
+++ src/pages/account/Profile.js
@@ -36,9 +36,15 @@ const ProfilePage = () => {
                         Thông Tin Tài Khoản
                     </h1>
                 </div>
-                
-                <div className="w-full h-[555px] bg-white rounded border">
-                    kk
+
+                <div className="w-full h-[555px] grid grid-cols-10 gap-5 bg-white rounded border">
+                    <div className="col-span-7 bg-gray-100 px-3 py-3">
+                        kk
+                    </div>
+
+                    <div className="col-span-3 bg-gray-200 px-3 py-3">
+                        kk
+                    </div>
                 </div>
             </div>
 
