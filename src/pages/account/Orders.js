f5cf9c3bc5d6341ab3c95534006e10061a717997
LeHoangSongDuy-B1909890 <duyb1909890@gmail.com>
2026-01-05 10:51:00 +0700

    Update new

--- src/pages/account/Orders.js
+++ src/pages/account/Orders.js
@@ -5,9 +5,20 @@ import Footer from "../../components/Footer";
 import Search from "../../components/Search";
 import Notification from "../../components/Notification";
 
+const tabs = [
+    "Tất cả đơn",
+    "Đang xử lý",
+    "Đang vận chuyển",
+    "Đã giao",
+    "Đã hủy",
+];
+
 const OrdersPage = () => {
     const [openSearch, setOpenSearch] = useState(false);
     const [openNotification, setOpenNotification] = useState(false);
+
+    // dnh sách đơn hàng
+    const [active, setActive] = useState(0);
 
     return (
         <>
@@ -38,12 +49,22 @@ const OrdersPage = () => {
                 </div>
 
                 <div className="w-full min-h-[555px] flex flex-col gap-3">
-                    <div className="w-full h-[45px] flex items-center px-3 rounded-md border bg-white">
-                        <ul className="w-full h-full">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                    <div className="w-full h-[45px] flex items-center rounded-md border bg-white">
+                        <ul className="w-full h-full grid grid-cols-5 items-center justify-center">
+                            {tabs.map((tab, index) => (
+                                <li
+                                    key={index}
+                                    className={`w-full h-full border-b-2 ${active === index ? "border-blue-500" : "border-transparent"
+                                        }`}
+                                >
+                                    <button
+                                        onClick={() => setActive(index)}
+                                        className="w-full h-full"
+                                    >
+                                        {tab}
+                                    </button>
+                                </li>
+                            ))}
                         </ul>
                     </div>
 
